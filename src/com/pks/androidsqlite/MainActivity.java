44f9dad9b1914362ae0309d519711936a55fc2e6
Syd Samonte <administrator@Peters-MacBook-Pro.local>
2015-01-12 12:37:16 +0800

    Added sql operations on MainActivity class

--- src/com/pks/androidsqlite/MainActivity.java
+++ src/com/pks/androidsqlite/MainActivity.java
@@ -1,22 +1,75 @@
 package com.pks.androidsqlite;
 
+import java.util.ArrayList;
 
-import android.app.Activity;
+import java.util.List;
+
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 
-public class MainActivity extends Activity {
-
-    @Override
+public class MainActivity extends ListActivity {
+	private StudentOperations studentDBoperation;
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }*/
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		studentDBoperation = new StudentOperations(this);
+		studentDBoperation.open();
 
-    @Override
+		List values = studentDBoperation.getAllStudents();
+
+		// Use the SimpleCursorAdapter to show the
+		// elements in a ListView
+		ArrayAdapter adapter = new ArrayAdapter(this,
+				android.R.layout.simple_list_item_1, values);
+		setListAdapter(adapter);
+	}
+	public void addUser(View view) {
+
+		ArrayAdapter adapter = (ArrayAdapter) getListAdapter();
+
+		EditText text = (EditText) findViewById(R.id.editText1);
+		Student stud = studentDBoperation.addStudent(text.getText().toString());
+
+		adapter.add(stud);
+
+	}
+	public void deleteFirstUser(View view) {
+
+		ArrayAdapter adapter = (ArrayAdapter) getListAdapter();
+		Student stud = null;
+
+		if (getListAdapter().getCount() > 0) {
+			stud = (Student) getListAdapter().getItem(0);
+			studentDBoperation.deleteStudent(stud);
+			adapter.remove(stud);
+		}
+
+	}
+
+	@Override
+	protected void onResume() {
+		studentDBoperation.open();
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		studentDBoperation.close();
+		super.onPause();
+	}
+
+   /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -33,5 +86,5 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
